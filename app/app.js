5a01e305d431e883e1d8525616844595ed74d239
stiles-micaiah <syluxprime40@gmail.com>
2019-07-29 15:25:29 -0600

    more work on my trash site

--- app/app.js
+++ app/app.js
@@ -1,6 +1,11 @@
 function copyText(str) {
   var dummyContent = str;
-  var dummy = $('<input>').val(dummyContent).appendTo('body').select()
-  document.execCommand('copy')
-  alert("Copied to clipboard!")
+  var dummy = $('<input>').val(dummyContent).appendTo('body').select();
+  document.execCommand('copy');
+  alert("Copied to clipboard!");
+}
+function searchRepo(query) {
+  event.preventDefault();
+  window.open("https://github.com/Stiles-Micaiah?utf8=✓&tab=repositories&q=" + query.value + "&type=&language=");
+  console.log(query);
 }
